b8fed0a732af47ad43338e7a805d1a98b9c600d6
Ben <bbryant@truecoders.io>
2021-01-07 12:16:20 -0600

    Added solution to Answer branch

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+function plus(number) {
+  return (plusNumber) => plusNumber + number;
+}
+
+let plus15 = plus(15);
+console.log(plus15(10));
